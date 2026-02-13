6b46b454b4a7754f3b61a061b98a4ed8a8da4630
xupeihao <xupeihao12$gmail.com>
2026-02-13 11:36:46 +0800

    4_1

--- User/main.c
+++ User/main.c
@@ -2,17 +2,27 @@
 #include "systick.h"
 #include <stdio.h>
 
+#include "gd32f4xx_rcu.h"
+#include "gd32f4xx_gpio.h"
+
 void GPIO_Config()
 {
-	
+	rcu_periph_clock_enable(RCU_GPIOB);
+    gpio_mode_set(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_2);
+    gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_2);
 }
 
 int main(void)
 {
 	systick_config();
-	
-    while(1) {
+	GPIO_Config();
 		
+    while(1) 
+	{
+		gpio_bit_write(GPIOB, GPIO_PIN_2, RESET);
+		delay_1ms(500);
+		gpio_bit_write(GPIOB, GPIO_PIN_2, SET);
+		delay_1ms(500);
     }
 }
 
